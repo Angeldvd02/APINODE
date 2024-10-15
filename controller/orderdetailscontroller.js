67074af20bf67548e3fc93dda847ac90d80c7044
angeldvd02 <daveipacheco@gmail.com>
2024-10-15 11:59:56 -0500

    archivo orderdetailscontroller editado

--- controller/orderdetailscontroller.js
+++ controller/orderdetailscontroller.js
@@ -17,4 +17,31 @@ ruta.get('/orderdetails', function(req, res) {
     });
 });
 
+// obtener orderdetails de una order
+ruta.get('/orders/:orderNumber/orderdetails', (req, res) => {
+    const { orderNumber } = req.params;
+    const sql = `SELECT * FROM orderdetails WHERE orderNumber = ${orderNumber}`;
+    conexion.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('No hay resultados');
+        }
+    });
+});
+
+// metodo para sumar todas las ordenes SUM(quantityOrdered*priceEach), de la orden 
+ruta.get('/orders/total/:orderNumber', (req, res) => {
+    const { orderNumber } = req.params;
+    const sql = `SELECT SUM(quantityOrdered*priceEach) as total FROM orderdetails WHERE orderNumber = ${orderNumber}`;
+    conexion.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('No hay resultados');
+        }
+    });
+});
 module.exports = ruta;
